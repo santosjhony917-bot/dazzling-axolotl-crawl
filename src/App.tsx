0a53955a191f12425c7354ab5f2617e8bc0e4668
[dyad] <santosjhony917@gmail.com>
2025-10-18 21:12:41 -0300

    [dyad] Adding a new splash screen - wrote 6 file(s), added framer-motion package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Splash from "./pages/Splash";
+import OnboardingEncontre from "./pages/OnboardingEncontre";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Splash />} />
+          <Route path="/home" element={<Index />} />
+          <Route path="/OnboardingEncontre" element={<OnboardingEncontre />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/OnboardingEncontre.tsx
@@ -0,0 +1,9 @@
+const OnboardingEncontre = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Onboarding Page</h1>
+    </div>
+  );
+};
+
+export default OnboardingEncontre;
