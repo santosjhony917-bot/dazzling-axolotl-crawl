7891b2ef52229f7a861ba5178de46aae4b0cc463
[dyad] <santosjhony917@gmail.com>
2025-10-23 08:07:57 -0300

    [dyad] wrote 1 file(s)

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -6,3 +6,8 @@ export const DEFAULT_RESTAURANT_LOGO_URL = `${SUPABASE_STORAGE_BASE_URL}/app-ass
 
 // URL para uma imagem placeholder genérica, agora vindo do Storage
 export const PLACEHOLDER_IMAGE_URL = `${SUPABASE_STORAGE_BASE_URL}/app-assets/placeholder.png`;
+
+// URLs para as imagens do onboarding
+export const ONBOARDING_IMAGE_1 = `${SUPABASE_STORAGE_BASE_URL}/app-assets/onboarding/onboarding-1.jpg`;
+export const ONBOARDING_IMAGE_2 = `${SUPABASE_STORAGE_BASE_URL}/app-assets/onboarding/onboarding-2.jpg`;
+export const ONBOARDING_IMAGE_3 = `${SUPABASE_STORAGE_BASE_URL}/app-assets/onboarding/onboarding-3.jpg`;
