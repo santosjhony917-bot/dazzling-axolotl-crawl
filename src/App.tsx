b60a0eb1af835029a54e0af1825d93d336882081
[dyad] <santosjhony917@gmail.com>
2025-10-25 16:33:12 -0300

    [dyad] Fixing BrowserRouter import error in App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ function App() {
           {/* Protected Restaurant Area Routes */}
           <Route path="/restaurant-area" element={<ProtectedRoute requiredRole="restaurant_owner" element={<RestaurantArea />} />}>
             <Route path="home" element={<RestaurantDashboardPage />} />
-            <Route path="stats" element={<ClientSearchPage />} />
+            <Route path="stats" element={<RestaurantSearch />} />
             <Route path="profile-menu" element={<RestaurantProfileMenu />} />
             <Route path="upgrade" element={<UpgradePage />} />
             <Route path="gallery" element={<GalleryManagement />} />
