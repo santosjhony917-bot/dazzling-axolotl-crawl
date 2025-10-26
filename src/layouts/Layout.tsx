8ceb28cc2188d5945056cef23c51fae60d1ed3db
[dyad] <santosjhony917@gmail.com>
2025-10-26 15:02:58 -0300

    [dyad] Melhorando a organização da área de conteúdo principal no layout. - wrote 1 file(s)

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-// Removendo importação de Toaster (sonner)
 
-const Layout: React.FC = () => {
+export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* O Outlet renderiza o componente da rota atual */}
-      <main className="flex-1">
+      <main className="flex-1 w-full mx-auto max-w-screen-xl">
         <Outlet />
       </main>
       {/* Toaster removido daqui, agora está em main.tsx */}
     </div>
   );
-};
-
-export default Layout;
+}
