4b9d693ac79dc372976f8e2d1f5c1ab423e31529
[dyad] <santosjhony917@gmail.com>
2025-11-10 07:19:55 -0300

    [dyad] Atualizando a importação do Supabase client na função de borda. - wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/bulk-create-restaurants/index.ts
+++ supabase/functions/bulk-create-restaurants/index.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0/dist/module.mjs';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
