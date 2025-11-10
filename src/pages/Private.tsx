4c52128d5f72e280e65bb7b12f99b7264f47c757
[dyad] <santosjhony917@gmail.com>
2025-11-10 19:07:44 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

---
+++ src/pages/Private.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Private = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Página Privada</h1>
+      <p>Este é um conteúdo que requer autenticação.</p>
+    </div>
+  );
+};
+
+export default Private;
